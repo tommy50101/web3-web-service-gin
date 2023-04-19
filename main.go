9594ceaa136d4bf74e2eecee3fd79019853fad67
tommy.c <tommy50101@gmail.com>
2023-04-19 23:56:18 +0800

    Get blocks and get block by id done

--- main.go
+++ main.go
@@ -6,46 +6,25 @@ import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"net/http"
+	// "net/http"
 	// "time"
 	"strconv"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-var myAlbum album
-var myAlbums []album
-
 type Block struct {
 	gorm.Model
-	BlockNum     uint   `gorm:"column:block_num"`
-	BlockHash    string `gorm:"column:block_hash"`
-	BlockTime    uint   `gorm:"column:block_time"` // 创建时间，时间戳
-	ParentHash   string `gorm:"column:parent_hash"`
-	Transactions []Transaction
+	BlockNum     uint          `gorm:"column:block_num"`
+	BlockHash    string        `gorm:"column:block_hash"`
+	BlockTime    uint          `gorm:"column:block_time"` // 创建时间，时间戳
+	ParentHash   string        `gorm:"column:parent_hash"`
+	Transactions []Transaction `gorm:"foreignKey:BlockID"`
 }
 type Transaction struct {
-	// gorm.Model
-	ID              uint   `gorm:"column:id"` // 主键
-	BlockID         uint   `gorm:"column:block_id"`
-	TransactionHash string `gorm:"column:transaction_hash"`
-	DeletedAt       string `gorm:"type:datetime;column:deleted_at"`
+	BlockID         uint // 預設foriegn key格式
+	TransactionHash string
 }
 
-type Result struct {
+type BlockRes struct {
 	BlockNum   uint
 	BlockHash  string
 	BlockTime  uint
@@ -53,11 +32,11 @@ type Result struct {
 }
 
 var block = Block{}
-var blocks = []Block{}
+var blockRes = BlockRes{}
+var getBlocksRes = []BlockRes{}
 
-var result = Result{}
+var txHashStrList []string
 
-// 设置表名，可以通过给struct类型定义 TableName函数，返回当前struct绑定的mysql表名是什么
 func (block Block) TableName() string {
 	// 绑定MYSQL表名为block
 	return "block"
@@ -82,32 +61,10 @@ var dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local
 // 连接MYSQL
 var db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-// if err != nil {
-// 	panic("连接数据库失败, error=" + err.Error())
-// }
-
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
 	router.GET("/blocks", getBlocks)
 	router.GET("/blocks/:id", getBlockByID)
-
-	// //定义一个用户，并初始化数据
-	// block := Block{
-	// 	BlockNum:   666,
-	// 	BlockHash:  "0x123456789",
-	// 	BlockTime:  time.Now().Unix(),
-	// 	ParentHash: "0x999999999",
-	// }
-
-	// //插入一条用户数据
-	// //自动生成SQL语句：INSERT INTO `block` (`block_num`,`block_hash`,`block_time`, `parent_hash`) VALUES (666,'0x123456789',date(),"0x999999999")
-	// if err := db.Create(&block).Error; err != nil {
-	// 	fmt.Println("插入失败", err)
-	// 	return
-	// }
 
 	router.Run("localhost:8080")
 }
@@ -116,65 +73,31 @@ func main() {
 func getBlocks(c *gin.Context) {
 	limit := c.Query("limit")
 	i, _ := strconv.Atoi(limit)
-	// result := Result{}
-	db.Model(&Block{}).Select("*").Joins("left join transaction on transaction.block_id = block.id").Limit(i).Find(&result)
-	// result := &Result{}
-	c.IndentedJSON(http.StatusOK, &result)
+	db.Model(&Block{}).Limit(i).Find(&getBlocksRes)
+
+	c.JSON(200, gin.H{
+		"blocks": &getBlocksRes,
+	})
 }
 
+// [GET] /blocks/:id
 func getBlockByID(c *gin.Context) {
-	//查询并返回第一条数据
-	//自动生成sql： SELECT * FROM `block`  WHERE (block_num = 666) LIMIT 1
 	id := c.Param("id")
 	result := db.Model(&Block{}).Preload("Transactions").Where("id = ?", id).First(&block)
-
 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
 		fmt.Println("找不到记录")
 		return
 	}
-	// c.IndentedJSON(http.StatusOK, &result)
+
+	for _, a := range block.Transactions {
+		txHashStrList = append(txHashStrList, a.TransactionHash)
+	}
 
 	c.JSON(200, gin.H{
 		"block_num":    block.BlockNum,
 		"block_hash":   block.BlockHash,
 		"block_time":   block.BlockTime,
 		"parent_hash":  block.ParentHash,
-		"transactions": block.Transactions,
+		"transactions": &txHashStrList,
 	})
-}
-
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
-// postAlbums adds an album from JSON received in the request body.
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
-
-	// Loop over the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
