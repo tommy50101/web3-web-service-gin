82dd236a29b4038634ccdc2df1fe06a27564d153
tommy50101 <tommy50101@gmail.com>
2023-04-30 13:20:39 +0800

    Add params to choose different chain

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -14,11 +17,31 @@ import (
 )
 
 var (
-	dsn string
-	db  *gorm.DB
+	dbName string
+	dsn    string
+	gormDb *gorm.DB
 )
 
 func main() {
+	// 判斷開啟哪個鏈的服務
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("請輸入要查詢哪個鏈上的資訊: 1.BSC testnet   2.Ethereum testnet(goerli) ")
+	fmt.Print("-> ")
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	if text[:len(text)-1] != "1" && text[:len(text)-1] != "2" {
+		log.Fatal("不合法的輸入")
+	}
+
+	// 依照輸入選擇不同庫
+	if text[:len(text)-1] == "1" {
+		dbName = "bsc_testnet"
+	} else {
+		dbName = "eth_testnet_goerli"
+	}
+
 	// 初始化DB
 	initDb()
 
@@ -32,8 +55,8 @@ func main() {
 
 func initDb() {
 	// 連線Db
-	dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", DB_USERNAME, DB_PWD, DB_HOST, DB_PORT, DB_NAME)
-	db, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", DB_USERNAME, DB_PWD, DB_HOST, DB_PORT, dbName)
+	gormDb, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 }
 
 // [GET] /blocks?limit=n
@@ -41,7 +64,7 @@ func getBlocks(c *gin.Context) {
 	blocks := []Block{}
 	limit := c.Query("limit")
 	i, _ := strconv.Atoi(limit)
-	db.Model(&Block{}).Limit(i).Order("block_num desc").Find(&blocks)
+	gormDb.Model(&Block{}).Limit(i).Order("block_num desc").Find(&blocks)
 
 	blocksRes := []BlockRes{}
 	for _, value := range blocks {
@@ -63,7 +86,7 @@ func getBlocks(c *gin.Context) {
 func getBlockByID(c *gin.Context) {
 	block := Block{}
 	id := c.Param("id")
-	result := db.Model(&Block{}).Preload("Transactions").Where("block_num = ?", id).First(&block)
+	result := gormDb.Model(&Block{}).Preload("Transactions").Where("block_num = ?", id).First(&block)
 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
 		fmt.Println("找不到紀錄")
 		return
@@ -88,7 +111,7 @@ func getBlockByID(c *gin.Context) {
 func getTxByTxHash(c *gin.Context) {
 	transaction := Transaction{}
 	txHash := c.Param("txHash")
-	result := db.Model(&Transaction{}).Preload("Logs").Where("tx_hash = ?", txHash).First(&transaction)
+	result := gormDb.Model(&Transaction{}).Preload("Logs").Where("tx_hash = ?", txHash).First(&transaction)
 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
 		fmt.Println("找不到紀錄")
 		return
